c11c5b747e3eba858eef869c52a47b5de114dd9c
nunopenim <nuno.penim@gmail.com>
2019-09-04 12:23:14 +0100

    webtools: pinging 1.1.1.1

--- userbot/modules/webtools.py
+++ userbot/modules/webtools.py
@@ -8,6 +8,7 @@
 
 from datetime import datetime
 
+import os
 import speedtest
 from telethon import functions
 from userbot import CMD_HELP
@@ -72,11 +73,12 @@ async def neardc(event):
 async def pingme(pong):
     """ FOr .pingme command, ping the userbot from any chat.  """
     if not pong.text[0].isalpha() and pong.text[0] not in ("/", "#", "@", "!"):
+		hostname = "1.1.1.1" #cloudfare's stable dns server
         start = datetime.now()
-        await pong.edit("`Pong!`")
+        response = os.system("ping -c 1 " + hostname)
         end = datetime.now()
         duration = (end - start).microseconds / 1000
-        await pong.edit("`Ping speed: %sms`" % (duration))
+        await pong.edit("Ping speed: %sms" % (duration))
 
 
 CMD_HELP.update(
