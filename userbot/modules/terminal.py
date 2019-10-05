087180b9c4baea4fa3149fc65262cc63aec16ef9
nunopenim <nuno.penim@gmail.com>
2019-10-05 17:41:38 +0100

    evaluators: renamed to terminal and removed eval command, too dangerous

--- userbot/modules/terminal.py
+++ userbot/modules/terminal.py
@@ -13,64 +13,6 @@ from sys import executable
 from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID
 from userbot.events import register, errors_handler
 
-
-@register(outgoing=True, pattern="^.eval(?: |$)(.*)")
-@errors_handler
-async def evaluate(query):
-    """ For .eval command, evaluates the given Python expression. """
-    if not query.text[0].isalpha() and query.text[0] not in ("/", "#", "@",
-                                                             "!"):
-        if query.is_channel and not query.is_group:
-            await query.edit("`Eval isn't permitted on channels`")
-            return
-
-        if query.pattern_match.group(1):
-            expression = query.pattern_match.group(1)
-        else:
-            await query.edit("``` Give an expression to evaluate. ```")
-            return
-
-        if expression in ("userbot.session", "config.env"):
-            await query.edit("`That's a dangerous operation! Not Permitted!`")
-            return
-
-        try:
-            evaluation = str(eval(expression))
-            if evaluation:
-                if isinstance(evaluation, str):
-                    if len(evaluation) >= 4096:
-                        file = open("output.txt", "w+")
-                        file.write(evaluation)
-                        file.close()
-                        await query.client.send_file(
-                            query.chat_id,
-                            "output.txt",
-                            reply_to=query.id,
-                            caption="`Output too large, sending as file`",
-                        )
-                        remove("output.txt")
-                        return
-                    await query.edit("**Query: **\n`"
-                                     f"{expression}"
-                                     "`\n**Result: **\n`"
-                                     f"{evaluation}"
-                                     "`")
-            else:
-                await query.edit("**Query: **\n`"
-                                 f"{expression}"
-                                 "`\n**Result: **\n`No Result Returned/False`")
-        except Exception as err:
-            await query.edit("**Query: **\n`"
-                             f"{expression}"
-                             "`\n**Exception: **\n"
-                             f"`{err}`")
-
-        if BOTLOG:
-            await query.client.send_message(
-                BOTLOG_CHATID,
-                f"Eval query {expression} was executed successfully")
-
-
 @register(outgoing=True, pattern=r"^.exec(?: |$)([\s\S]*)")
 @errors_handler
 async def run(run_q):
