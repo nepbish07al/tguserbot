3adf9a98b3a0a89baa9379e361433683c5579182
nunopenim <nuno.penim@gmail.com>
2019-10-05 23:18:01 +0100

    webtools: fixed ping

--- userbot/modules/webtools.py
+++ userbot/modules/webtools.py
@@ -3,16 +3,14 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-# module renamed webtools, ping possibly modified, rtt added
 """ Userbot module containing commands related to the \
     Information Superhighway(yes, Internet). """
 
 from datetime import datetime
 
-import os
 import speedtest
-import subprocess
 from telethon import functions
+from subprocess import check_output
 from userbot import CMD_HELP
 from userbot.events import register, errors_handler
 
@@ -21,15 +19,14 @@ from userbot.events import register, errors_handler
 @errors_handler
 async def speedtst(spd):
     """ For .speed command, use SpeedTest to check server speeds. """
-    if not spd.text[0].isalpha() and spd.text[0] not in ("/", "#", "@", "!"):
-        await spd.edit("`Running speed test . . .`")
-        test = speedtest.Speedtest()
+    await spd.edit("`Running speed test . . .`")
+    test = speedtest.Speedtest()
 
-        test.get_best_server()
-        test.download()
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
 
     await spd.edit("`"
                    "Started at "
@@ -58,41 +55,41 @@ def speed_convert(size):
     return f"{round(size, 2)} {units[zero]}"
 
 
-@register(outgoing=True, pattern="^.neardc$")
+@register(outgoing=True, pattern="^.dc$")
 @errors_handler
 async def neardc(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@",
-                                                             "!"):
-        """ For .nearestdc command, get the nearest datacenter information. """
-        result = await event.client(functions.help.GetNearestDcRequest())
-        await event.edit(f"Country : `{result.country}` \n"
-                         f"Nearest Datacenter : `{result.nearest_dc}` \n"
-                         f"This Datacenter : `{result.this_dc}`")
+    """ For .dc command, get the nearest datacenter information. """
+    result = await event.client(functions.help.GetNearestDcRequest())
+    await event.edit(f"Country : `{result.country}`\n"
+                     f"Nearest Datacenter : `{result.nearest_dc}`\n"
+                     f"This Datacenter : `{result.this_dc}`")
 
 
 @register(outgoing=True, pattern="^.ping$")
 @errors_handler
 async def pingme(pong):
     """ For .ping command, ping the userbot to Telegram server from any chat.  """
-    duration = check_output("ping -c 1 1.0.0.1 | grep -oP '.*time=\K(\d*\.\d*).*'", shell=True).decode()
-    await ping.edit("`Round-trip time\n%s`" % (duration))
+    start = datetime.now()
+    await pong.edit("`Pinging...`")
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await pong.edit("`Ping to Telegram server\n%sms`" % (duration)) 
 
-#Kanged from @prototype74's userbot        
+        
 @register(outgoing=True, pattern="^.rtt$")
 @errors_handler
 async def rtt(ping):
     """ For .rtt command, get current round-trip time from any chat.  """
-    duration = check_output("ping -c 1 1.0.0.1 | grep -oP '.*time=\K(\d*\.\d*).*'", shell=True).decode()
+    duration = check_output("ping -c 1 1.1.1.1 | grep -oP '.*time=\K(\d*\.\d*).*'", shell=True).decode()
     await ping.edit("`Round-trip time\n%s`" % (duration))
+
 
 CMD_HELP.update(
     {"speed": ".speed\
     \nUsage: Does a speedtest and shows the results."})
-CMD_HELP.update({
-    "nearestdc":
-    ".nearestdc\
-    \nUsage: Finds the nearest datacenter from your server."
-})
+CMD_HELP.update(
+    {"dc": ".dc\
+    \nUsage: Finds the nearest datacenter from your server."})
 CMD_HELP.update(
     {"ping": ".ping\
     \nUsage: Shows how long it takes to ping the Telegram server."})
