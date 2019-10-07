5df8b137fe286f6cdd54f3ea5ffcb88f2c0b8bb6
nunopenim <nuno.penim@gmail.com>
2019-10-07 14:20:40 +0100

    webtools: introduced cping

--- userbot/modules/webtools.py
+++ userbot/modules/webtools.py
@@ -82,8 +82,8 @@ async def cping(args):
             await args.edit("`Bad arguments!`")
         else:
             dns = commandParser[1]
-            #duration = check_output("ping -c 1 1.0.0.1 | grep -oP '.*time=\K(\d*\.\d*).*'", shell=True).decode()
-            await args.edit("`DNS: " + dns +"`")
+            duration = check_output("ping -c 1 "+dns+" | grep -oP '.*time=\K(\d*\.\d*).*'", shell=True).decode()
+            await args.edit("`DNS: " + dns +"\n"+"Ping speed: "+duration)
 
 CMD_HELP.update(
     {"speed": ".speed\
