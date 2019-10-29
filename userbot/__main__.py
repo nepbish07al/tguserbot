548d29dc776490f477ae8ac1cce464ea22b864cd
Nuno Penim <nuno.penim@gmail.com>
2019-10-29 12:01:40 +0000

    main: cleanup

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,13 +1,5 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot start point """
-
 from importlib import import_module
 from sys import argv
-
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import LOGS, bot
 from userbot.modules import ALL_MODULES
@@ -26,10 +18,10 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("Your userbot version is 4.0 - Essentials")
+LOGS.info("You are running tguserbot - 1.0")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .status in any chat.")
+    "Userbot is running, test it by typing .status")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
