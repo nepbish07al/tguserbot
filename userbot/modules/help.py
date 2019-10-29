82db9fd22cfa397353ea4bf69061c7d49f1cc651
Nuno Penim <nuno.penim@gmail.com>
2019-10-29 12:05:43 +0000

    help: cleanup

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,13 +1,5 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot help command """
-
 from userbot import CMD_HELP
 from userbot.events import register, errors_handler
-
 
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
 @errors_handler
