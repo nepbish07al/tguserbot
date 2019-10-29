610e25585c10c3e1830a3ce59b576eff9dc317d3
Nuno Penim <nuno.penim@gmail.com>
2019-10-29 12:08:21 +0000

    purge: cleanup

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -1,17 +1,7 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for purging unneeded messages(usually spam or ot). """
-
 from asyncio import sleep
-
 from telethon.errors import rpcbaseerrors
-
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register, errors_handler
-
 
 @register(outgoing=True, pattern="^.purge$")
 @errors_handler
