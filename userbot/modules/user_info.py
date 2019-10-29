f807fe88cae8c027a7518f933543d063e4dcec7b
nunopenim <nuno.penim@gmail.com>
2019-10-29 19:56:24 +0000

    user_info: code restructure

--- userbot/modules/user_info.py
+++ userbot/modules/user_info.py
@@ -8,81 +8,51 @@ from userbot.events import register, errors_handler
 
 @register(pattern=".info(?: |$)(.*)", outgoing=True)
 @errors_handler
-async def who(event):
+async def who(event): #.info command
     if not event.text[0].isalpha() and event.text[0] in ("."):
-        """ For .whois command, get info about a user. """
         if event.fwd_from:
             return
-
-        await event.edit(
-            "`Sit tight while I steal some data from Mark Zuckerburg...`")
-
+        await event.edit("`Calling Mark Zuckerburg...`")
         if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
             os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-
         replied_user = await get_user(event)
-
         photo, caption = await fetch_info(replied_user, event)
-
         message_id_to_reply = event.message.reply_to_msg_id
-
         if not message_id_to_reply:
             message_id_to_reply = None
-
         try:
-            await event.client.send_file(event.chat_id,
-                                         caption=caption,
-                                         link_preview=False,
-                                         force_document=False,
-                                         reply_to=message_id_to_reply,
-                                         parse_mode="html")
-
+            await event.client.send_file(event.chat_id, caption=caption, link_preview=False, force_document=False, reply_to=message_id_to_reply, parse_mode="html")
             await event.delete()
-
         except TypeError:
             await event.edit(caption, parse_mode="html")
 
-async def get_user(event):
-    """ Get the user from argument or replied message. """
+async def get_user(event): #fetch user, Zuckerburg's phone call
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
-        replied_user = await event.client(
-            GetFullUserRequest(previous_message.from_id))
+        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
     else:
         user = event.pattern_match.group(1)
-
         if user.isnumeric():
             user = int(user)
-
         if not user:
             self_user = await event.client.get_me()
             user = self_user.id
-
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
-
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 replied_user = await event.client(GetFullUserRequest(user_id))
                 return replied_user
         try:
             user_object = await event.client.get_entity(user)
-            replied_user = await event.client(
-                GetFullUserRequest(user_object.id))
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
         except (TypeError, ValueError) as err:
             await event.edit(str(err))
             return None
-
     return replied_user
 
 async def fetch_info(replied_user, event):
-    """ Get details from the User object. """
-    replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(user_id=replied_user.user.id,
-                             offset=42,
-                             max_id=0,
-                             limit=80))
+    replied_user_profile_photos = await event.client(GetUserPhotosRequest(user_id=replied_user.user.id, offset=42, max_id=0,limit=80))
     replied_user_profile_photos_count = "Person needs help with uploading profile picture."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
@@ -102,18 +72,11 @@ async def fetch_info(replied_user, event):
     is_bot = replied_user.user.bot
     restricted = replied_user.user.restricted
     verified = replied_user.user.verified
-    photo = await event.client.download_profile_photo(user_id,
-                                                      TEMP_DOWNLOAD_DIRECTORY +
-                                                      str(user_id) + ".jpg",
-                                                      download_big=True)
-    first_name = first_name.replace(
-        "\u2060", "") if first_name else ("This User has no First Name")
-    last_name = last_name.replace(
-        "\u2060", "") if last_name else ("This User has no Last Name")
-    username = "@{}".format(username) if username else (
-        "This User has no Username")
+    photo = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg", download_big=True)
+    first_name = first_name.replace("\u2060", "") if first_name else ("This User has no First Name")
+    last_name = last_name.replace("\u2060", "") if last_name else ("This User has no Last Name")
+    username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
-
     caption = "<b>USER INFO:</b>\n\n"
     caption += f"First Name: {first_name}\n"
     caption += f"Last Name: {last_name}\n"
@@ -128,11 +91,9 @@ async def fetch_info(replied_user, event):
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
     caption += f"Common Chats with this user: {common_chat}\n"
-
     return photo, caption
 
 CMD_HELP.update({
     "info":
     ".info <username> or reply to someones text with .whois\
-    \nUsage: Gets info of an user."
-})
+    \nUsage: Gets info of an user."})
