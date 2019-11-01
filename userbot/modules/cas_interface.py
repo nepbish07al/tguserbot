aec5d8a5300f60569f2a1970782c9b5f9c7bbee4
Nuno Penim <nuno.penim@gmail.com>
2019-11-01 16:57:36 +0000

    cas_interface: added checks for . only

--- userbot/modules/cas_interface.py
+++ userbot/modules/cas_interface.py
@@ -14,90 +14,91 @@ from userbot.events import errors_handler, register
 @register(outgoing=True, pattern="^.cascheck ?(.*)")
 @errors_handler
 async def cascheck(cas): #checks if a user, or all users in a group are cas banned
-    if cas.reply_to_msg_id:
-        replied_msg = await cas.get_reply_message()
-        chat = replied_msg.from_id
-    else:
-        chat = cas.pattern_match.group(1)
-        if chat:
-            try:
-                chat = int(chat)
-            except ValueError:
-                pass
-    if not chat:
-        chat = cas.chat_id
-    try:
-        info = await cas.client.get_entity(chat)
-    except (TypeError, ValueError) as err:
-        await cas.edit(str(err))
-        return
-    exportcsv = TEMP_DOWNLOAD_DIRECTORY + "/export.csv"
-    user_ids = []
-    if path.exists(exportcsv):
-        if isCSVoutdated():
-            await cas.edit("`Auto updating CAS data...`")
-            print("CASCHECK: Auto update CAS data started")
-            await casupdater(cas, showinfo=False)
-        await cas.edit("`Processing...`")
-        with open(exportcsv) as data:
-            for cas_id in data:
-                user_ids.append(int(cas_id))
-    else:
-        await cas.edit("`CAS data not found. Please use .casupdate command to get the latest CAS data`")
-        return
-    try:
-        if type(info) is User:  # check an user only
-            if info.id in user_ids:
-                if not info.deleted:
-                    text = f"Warning! [{info.first_name}](tg://user?id={info.id}) [ID: `{info.id}`] is CAS Banned!"
-                else:
-                    text = f"Warning! Deleted Account [ID: `{info.id}`] is CAS Banned!"
-            else:
-                text = f"[{info.first_name}](tg://user?id={info.id}) is not CAS Banned"
-        else:  # check for all members in a chat
-            title = info.title if info.title else "this chat"
-            cas_count, members_count = (0,)*2
-            text_users = ""
-            async for user in cas.client.iter_participants(info.id):
-                if user.id in user_ids:
-                    cas_count += 1
-                    if not user.deleted:
-                        text_users += f"\n{cas_count}. [{user.first_name}](tg://user?id={user.id}) `{user.id}`"
-                    else:
-                        text_users += f"\n{cas_count}. Deleted Account `{user.id}`"
-                members_count += 1
-            text = "Warning! `{}` of `{}` users are CAS Banned in **{}**:\n".format(cas_count, members_count, title)
-            text += text_users
-            if not cas_count:
-                text = f"`No CAS Banned users found in {title}`"
-    except ChatAdminRequiredError as carerr:
-        await cas.edit("`CAS check failed: Admin privileges are required`")
-        print("ChatAdminRequiredError:", carerr)
-        return
-    except BaseException as be:
-        await cas.edit("`CAS check failed`")
-        print("BaseException:", be)
-        return
-    finally:
-        data.close()
-    try:
-        await cas.edit(text)
-    except MessageTooLongError as mtle:
-        print("MessageTooLongError:", mtle)
-        await cas.edit("`Jesus christ, there are too many CAS Banned users in this chat. Uploading list as a file...`")
-        temp_file = open("caslist.txt", "w+")
-        temp_file.write(text)
-        temp_file.close()
+    if not cas.text[0].isalpha() and cas.text[0] in ("."):
+        if cas.reply_to_msg_id:
+            replied_msg = await cas.get_reply_message()
+            chat = replied_msg.from_id
+        else:
+            chat = cas.pattern_match.group(1)
+            if chat:
+                try:
+                    chat = int(chat)
+                except ValueError:
+                    pass
+        if not chat:
+            chat = cas.chat_id
         try:
-            await cas.client.send_file(cas.chat_id, "caslist.txt")
-        except ChatSendMediaForbiddenError as f:
-            await cas.edit("`Failed to upload list: send media isn't allowed in this chat.`")
-            print("ChatSendMediaForbiddenError:", f)
+            info = await cas.client.get_entity(chat)
+        except (TypeError, ValueError) as err:
+            await cas.edit(str(err))
+            return
+        exportcsv = TEMP_DOWNLOAD_DIRECTORY + "/export.csv"
+        user_ids = []
+        if path.exists(exportcsv):
+            if isCSVoutdated():
+                await cas.edit("`Auto updating CAS data...`")
+                print("CASCHECK: Auto update CAS data started")
+                await casupdater(cas, showinfo=False)
+            await cas.edit("`Processing...`")
+            with open(exportcsv) as data:
+                for cas_id in data:
+                    user_ids.append(int(cas_id))
+        else:
+            await cas.edit("`CAS data not found. Please use .casupdate command to get the latest CAS data`")
+            return
+        try:
+            if type(info) is User:  # check an user only
+                if info.id in user_ids:
+                    if not info.deleted:
+                        text = f"Warning! [{info.first_name}](tg://user?id={info.id}) [ID: `{info.id}`] is CAS Banned!"
+                    else:
+                        text = f"Warning! Deleted Account [ID: `{info.id}`] is CAS Banned!"
+                else:
+                    text = f"[{info.first_name}](tg://user?id={info.id}) is not CAS Banned"
+            else:  # check for all members in a chat
+                title = info.title if info.title else "this chat"
+                cas_count, members_count = (0,)*2
+                text_users = ""
+                async for user in cas.client.iter_participants(info.id):
+                    if user.id in user_ids:
+                        cas_count += 1
+                        if not user.deleted:
+                            text_users += f"\n{cas_count}. [{user.first_name}](tg://user?id={user.id}) `{user.id}`"
+                        else:
+                            text_users += f"\n{cas_count}. Deleted Account `{user.id}`"
+                    members_count += 1
+                text = "Warning! `{}` of `{}` users are CAS Banned in **{}**:\n".format(cas_count, members_count, title)
+                text += text_users
+                if not cas_count:
+                    text = f"`No CAS Banned users found in {title}`"
+        except ChatAdminRequiredError as carerr:
+            await cas.edit("`CAS check failed: Admin privileges are required`")
+            print("ChatAdminRequiredError:", carerr)
+            return
         except BaseException as be:
-            await cas.edit("`Failed to upload list`")
+            await cas.edit("`CAS check failed`")
             print("BaseException:", be)
-        remove("caslist.txt")
-        return
+            return
+        finally:
+            data.close()
+        try:
+            await cas.edit(text)
+        except MessageTooLongError as mtle:
+            print("MessageTooLongError:", mtle)
+            await cas.edit("`Jesus christ, there are too many CAS Banned users in this chat. Uploading list as a file...`")
+            temp_file = open("caslist.txt", "w+")
+            temp_file.write(text)
+            temp_file.close()
+            try:
+                await cas.client.send_file(cas.chat_id, "caslist.txt")
+            except ChatSendMediaForbiddenError as f:
+                await cas.edit("`Failed to upload list: send media isn't allowed in this chat.`")
+                print("ChatSendMediaForbiddenError:", f)
+            except BaseException as be:
+                await cas.edit("`Failed to upload list`")
+                print("BaseException:", be)
+            remove("caslist.txt")
+            return
 
 def isCSVoutdated() -> bool: #checks if csv is a day or more old
     csv_file = TEMP_DOWNLOAD_DIRECTORY + "/export.csv"
@@ -113,8 +114,9 @@ def isCSVoutdated() -> bool: #checks if csv is a day or more old
 @register(outgoing=True, pattern="^.casupdate$")
 @errors_handler
 async def casupdate(event): #updates cas csv
-    await casupdater(event, showinfo=True)
-    return
+    if not target.text[0].isalpha() and target.text[0] in ("."):
+        await casupdater(event, showinfo=True)
+        return
 
 async def casupdater(down, showinfo: bool): #csv downloader
     url = "https://combot.org/api/cas/export.csv"
