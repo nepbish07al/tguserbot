40553650528440cc06079e53b72c15dc5254adf4
Nuno Penim <nuno.penim@gmail.com>
2020-05-19 10:41:05 +0100

    spam: added delayspam

--- tg_userbot/modules/spam.py
+++ tg_userbot/modules/spam.py
@@ -26,10 +26,26 @@ async def spammer(e):
         if BOTLOG:
             await e.client.send_message(BOTLOG_CHATID, "#SPAM \n\nSpam was executed successfully")
 
+@register(outgoing=True, pattern="^.delayspam (.*)")
+@errors_handler
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] in ("."):
+        spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
+        counter = int(e.pattern_match.group(1).split(' ', 2)[1])
+        spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
+        await e.delete()
+        for i in range(1, counter):
+            await e.respond(spam_message)
+            await sleep(spamDelay)
+        if BOTLOG:
+            await e.client.send_message(BOTLOG_CHATID, "#DelaySPAM\n\nDelaySpam was executed successfully")
+
 CMD_HELP.update({
     "spam":
     ".tspam <text>\
 \nUsage: Spam the text letter by letter.\
 \n\n.spam <count> <text>\
 \nUsage: Floods text in the chat !!\
+\n\n.delayspam <delay> <count> <text>\
+\nUsage: spams with a time delay.\
 \n\n\nNOTE : Spam at your own risk !!"})
