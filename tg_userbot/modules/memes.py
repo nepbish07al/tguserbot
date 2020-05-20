17d825ee5220e3a8637a1059f60e08bbd777645f
Nuno Penim <nuno.penim@gmail.com>
2020-05-20 18:49:49 +0100

    spam: typewriter only triggers with . now

--- tg_userbot/modules/memes.py
+++ tg_userbot/modules/memes.py
@@ -584,29 +584,28 @@ async def scam(event):
 @register(pattern=r".type(?: |$)(.*)", outgoing=True)
 @errors_handler
 async def typewriter(typew):
-    """ Just a small command to make your keyboard become a typewriter! """
-    textx = await typew.get_reply_message()
-    message = typew.pattern_match.group(1)
-    if message:
-        pass
-    elif textx:
-        message = textx.text
-    else:
-        await typew.edit("`Give a text to type!`")
-        return
-    sleep_time = 0.1
-    typing_symbol = "|"
-    old_text = ""
-    await typew.edit(typing_symbol)
-    await asyncio.sleep(sleep_time)
-    for character in message:
-        old_text = old_text + "" + character
-        typing_text = old_text + "" + typing_symbol
-        await typew.edit(typing_text)
+    if not typew.text[0].isalpha() and typew.text[0] in ("."):
+        textx = await typew.get_reply_message()
+        message = typew.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await typew.edit("`Give a text to type!`")
+            return
+        sleep_time = 0.1
+        typing_symbol = "|"
+        old_text = ""
+        await typew.edit(typing_symbol)
         await asyncio.sleep(sleep_time)
-        await typew.edit(old_text)
-        await asyncio.sleep(sleep_time)
-
+        for character in message:
+            old_text = old_text + "" + character
+            typing_text = old_text + "" + typing_symbol
+            await typew.edit(typing_text)
+            await asyncio.sleep(sleep_time)
+            await typew.edit(old_text)
+            await asyncio.sleep(sleep_time)
 
 CMD_HELP.update({
     "memes":
