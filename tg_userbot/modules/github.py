53badfb4238e7e36df9f207f677e35b8427c2353
Nuno Penim <nuno.penim@gmail.com>
2020-06-06 17:27:29 +0100

    general: introduced github module

---
+++ tg_userbot/modules/github.py
@@ -0,0 +1,41 @@
+import tg_userbot.modules.libs.git_api as api
+from tg_userbot.events import register, errors_handler
+
+#do not async
+def getData(url, index):
+    if not api.getData(url):
+        return "Invalid user/repo combo"
+    recentRelease = api.getReleaseData(api.getData(url), index)
+    if recentRelease is None:
+        return "The specified release could not be found"
+    author = api.getAuthor(recentRelease)
+    authorUrl = api.getAuthorUrl(recentRelease)
+    assets = api.getAssets(recentRelease)
+    releaseName = api.getReleaseName(recentRelease)
+    message = "<b>Author:</b> <a href='{}'>{}</a>\n".format(authorUrl, author)
+    message += "<b>Release Name:</b> "+releaseName+"\n\n"
+    for asset in assets:
+        message += "<b>Asset:</b> \n"
+        fileName = api.getReleaseFileName(asset)
+        fileURL = api.getReleaseFileURL(asset)
+        assetFile = "<a href='{}'>{}</a>".format(fileURL, fileName)
+        sizeB = ((api.getSize(asset))/1024)/1024
+        size = "{0:.2f}".format(sizeB)
+        downloadCount = api.getDownloadCount(asset)
+        message += assetFile + "\n"
+        message += "Size: " + size + " MB"
+        message += "\nDownload Count: " + str(downloadCount) + "\n\n"
+    return message
+
+@register(pattern=".git(?: |$)(.*)", outgoing=True)
+@errors_handler
+async def get_release(event):
+    if not event.text[0].isalpha() and event.text[0] in ("."):
+        commandArgs = event.text.split(" ")
+        if len(commandArgs) != 2 or not "/" in commandArgs[1]:
+            await event.edit("Invalid arguments! Make sure you are typing a valid combination of user/repo")
+            return
+        index = 0 #for now...
+        url = commandArgs[1]
+        text = getData(url, index)
+        await event.edit(text, parse_mode="html")
