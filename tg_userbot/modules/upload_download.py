0fa9dfc0b8e0fcbc582cf0873cf1db9af54784a6
HitaloSama <hitalomarquete331@gmail.com>
2020-06-07 14:08:10 -0300

    upload_download: base upstream (PaperplaneExtended)

--- tg_userbot/modules/upload_download.py
+++ tg_userbot/modules/upload_download.py
@@ -1,16 +1,24 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The entire source code is OSSRPL except
+# 'download, uploadir, uploadas, upload' which is MPL
+# License: MPL and OSSRPL
 """ Userbot module which contains everything related to \
     downloading/uploading from/to the server. """
 
-import asyncio
 import json
-import math
 import os
 import subprocess
 import time
+import math
 
+from pySmartDL import SmartDL
+import asyncio
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo
 
 from tg_userbot import LOGS, CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
@@ -28,15 +36,15 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         progress_str = "[{0}{1}] {2}%\n".format(
-            ''.join(["▰" for i in range(math.floor(percentage / 5))]),
-            ''.join(["▱" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["█" for i in range(math.floor(percentage / 10))]),
+            ''.join(["░" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
         tmp = progress_str + \
-              "{0} of {1}\nETA: {2}".format(
-                  humanbytes(current),
-                  humanbytes(total),
-                  time_formatter(estimated_total_time)
-              )
+            "{0} of {1}\nETA: {2}".format(
+                humanbytes(current),
+                humanbytes(total),
+                time_formatter(estimated_total_time)
+            )
         if file_name:
             await event.edit("{}\nFile Name: `{}`\n{}".format(
                 type_of_ps, file_name, tmp))
@@ -51,7 +59,7 @@ def humanbytes(size):
     if not size:
         return ""
     # 2 ** 10 = 1024
-    power = 2 ** 10
+    power = 2**10
     raised_to_pow = 0
     dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
     while size > power:
@@ -68,10 +76,10 @@ def time_formatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = ((str(days) + " day(s), ") if days else "") + \
-          ((str(hours) + " hour(s), ") if hours else "") + \
-          ((str(minutes) + " minute(s), ") if minutes else "") + \
-          ((str(seconds) + " second(s), ") if seconds else "") + \
-          ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+        ((str(hours) + " hour(s), ") if hours else "") + \
+        ((str(minutes) + " minute(s), ") if minutes else "") + \
+        ((str(seconds) + " second(s), ") if seconds else "") + \
+        ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
     return tmp[:-2]
 
 
@@ -107,8 +115,9 @@ async def download(target_file):
             speed = downloader.get_speed()
             elapsed_time = round(diff) * 1000
             progress_str = "[{0}{1}] {2}%".format(
-                ''.join(["▰" for i in range(math.floor(percentage / 5))]),
-                ''.join(["▱" for i in range(20 - math.floor(percentage / 5))]),
+                ''.join(["█" for i in range(math.floor(percentage / 10))]),
+                ''.join(["░"
+                         for i in range(10 - math.floor(percentage / 10))]),
                 round(percentage, 2))
             estimated_total_time = downloader.get_eta(human=True)
             try:
@@ -395,9 +404,9 @@ async def uploadas(uas_event):
 
 
 CMD_HELP.update({
-    "download_upload":
-        ".download <link|filename> or reply to media\
-    \nUsage: Downloads file to the server.\
-    \n\n.upload <path in server>\
-    \nUsage: Uploads a locally stored file to the chat."
+    "upload_download":
+    "`.download <link|filename>` or reply to media\
+\nUsage: Downloads file to the server.\
+\n\n`.upload <path in server>`\
+\nUsage: Uploads a locally stored file to the chat."
 })
